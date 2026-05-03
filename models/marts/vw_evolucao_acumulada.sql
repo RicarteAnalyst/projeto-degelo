2caed1240d4d55988d8b01ab22fcaca0b9ca4807
Nicollas Ricarte <nicollasricarte1212@gmail.com>
2026-05-03 20:25:41 +0000

    update

--- models/marts/vw_evolucao_acumulada.sql
+++ models/marts/vw_evolucao_acumulada.sql
@@ -1,7 +1,6 @@
 SELECT
-  AVG(f.Variacao_Sazonal) AS Evolucao_Acumulada,
-  d.Ano
-FROM {{ ref('fct_massa_gelo') }} f
-INNER JOIN {{ ref('dim_data') }} d ON f.Data = d.Data
-GROUP BY d.Ano
-ORDER BY d.Ano
+  AVG(Variacao_Sazonal) AS Evolucao_Acumulada,
+  EXTRACT(YEAR FROM Data) AS Ano
+FROM {{ ref('fct_massa_gelo') }}
+GROUP BY Ano
+ORDER BY Ano
